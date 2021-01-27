6c9928bfbb5eb7e31d0340e67c953779d9428a2d
root <jin.xin@advantech.com.cn>
2021-01-27 09:27:33 +0800

    release image: 1.2.0-1-debug

--- build-debug.sh
+++ build-debug.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 TARGET=fluent-bit
-REPO=registry.cn-shanghai.aliyuncs.com/advantech-k8s/
-TAG=1.2.0-debug
+#REPO=registry.cn-shanghai.aliyuncs.com/advantech-k8s/
+REPO=harbor.arfa.wise-paas.com/ensaas-logging/
+TAG=1.2.0-1-debug
 docker build -t ${REPO}${TARGET}:${TAG} -f ./Dockerfile.debug .
 docker push ${REPO}${TARGET}:${TAG}
 
